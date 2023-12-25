d358b99b38d357a20b634645db7ee29a90d295de
SaiAshish <saiashish3760@gmail.com>
2023-12-25 15:12:46 +0530

    k

--- src/main/java/com/sai/ScrambleString.java
+++ src/main/java/com/sai/ScrambleString.java
@@ -4,6 +4,49 @@ import java.util.Arrays;
 
 public class ScrambleString {
 
+       // dp[i][j][len]: represents whether the substring of length len starting from index i in s1 and the substring of length len starting from index j in s2 are scrambled strings.
+    public boolean isScramble1(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int n = s1.length();
+        boolean[][][] dp = new boolean[n][n][n + 1];
+
+        // strings of length 1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j][1] = true;
+                }
+            }
+        }
+
+        // longer substrings
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                for (int j = 0; j <= n - len; j++) {
+                    for (int k = 1; k < len; k++) {
+                        if ((dp[i][j][k] && dp[i + k][j + k][len - k]) // without swap
+                                || (dp[i][j + len - k][k] && dp[i + k][j][len - k]) // with swap
+                        ) {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.deepToString(dp)
+                .replaceAll("]]", "]\n]")
+                .replaceAll("\\[\\[", "[\n[")
+                .replaceAll("\\[\\[", "[\n[")
+                .replaceAll("],", "],\n")
+                .replaceAll("]]", "]\n]")
+        );
+        return dp[0][0][n];
+    }
+
     public static boolean isScramble(String s1, String s2) {
         int n = s1.length();
         if (n != s2.length()) {
