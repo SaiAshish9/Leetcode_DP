60fb59b9440bd4311d0da7869e67178ec12e474f
SaiAshish <saiashish3760@gmail.com>
2024-04-05 16:19:19 +0530

    k

--- BestTimeToBuyAndSellStock2.java
+++ BestTimeToBuyAndSellStock2.java
@@ -31,6 +31,34 @@ public class BestTimeToBuyAndSellStock2 {
         return Math.max(dp[n - 1][0], dp[n - 1][1]);
     }
 
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock.
+//  You can only hold at most one share of the stock at any time. 
+// However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+
+ public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        int[] dp = new int[n];
+
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            int diff = prices[i] - prices[i - 1];
+            dp[i] = Math.max(dp[i - 1] + diff, dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+    // dp array stores the maximum profit at each day. 
+
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{7,1,5,3,6,4})); // 0
     }
