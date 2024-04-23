ecff71e7daf09af59eeae4bb1a69442953a870d6
SaiAshish <saiashish3760@gmail.com>
2024-04-23 23:04:28 +0530

    k

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -16,10 +16,12 @@ public class MaximalSquare {
                 if (i == 0 || j == 0 || matrix[i][j] == '0')
                     dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
                 else
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],
+                     Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 maxLength = Math.max(maxLength, dp[i][j]);
             }
-        System.out.println(Arrays.deepToString(dp).replaceAll("],", "],\n"));
+        System.out.println(Arrays.deepToString(dp)
+        .replaceAll("],", "],\n"));
         return maxLength * maxLength;
     }
 
